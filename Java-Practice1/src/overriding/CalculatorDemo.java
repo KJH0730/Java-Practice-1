253fcadd243ceef3a3eafb680ddced45681fd1b1
Junghwan Kim <junghwankim@dongyang.ac.kr>
2023-03-13 14:31:41 +0900

    Modified 'Calculator for overriding'

--- Java-Practice1/src/overriding/CalculatorDemo.java
+++ Java-Practice1/src/overriding/CalculatorDemo.java
@@ -2,27 +2,31 @@ package overriding;
 
 class Calculator {
 	int left, right;
-	
+
 	public void setOprands(int left, int right) {
 		this.left = left;
 		this.right = right;
 	}
-	
+
 	public void sum() {
 		System.out.println(this.left + this.right);
 	}
-	
-	public void avg() {
-		System.out.println((this.left + this.right) / 2);
+
+	public int avg() {
+		return ((this.left + this.right) / 2);
 	}
 }
 
 class SubstractionableCalculator extends Calculator {
-	
+
 	public void sum() {
 		System.out.println("실행 결과는 " + (this.left + this.right) + "입니다.");
 	}
+
 	
+	  public int avg() { return super.avg(); }
+	 
+
 	public void substract() {
 		System.out.println(this.left - this.right);
 	}
@@ -34,7 +38,7 @@ public class CalculatorDemo {
 		SubstractionableCalculator c1 = new SubstractionableCalculator();
 		c1.setOprands(10, 20);
 		c1.sum();
-		c1.avg();
+		System.out.println("실행 결과는" + c1.avg());
 		c1.substract();
 	}
 
